0c92362e571e7106a2d4886c70a7a86516e34933
saida javid <sadiajavid31@gmail.com>
2026-01-05 10:46:46 +0500

    filter by menus

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,11 +9,11 @@ const FoodDisplay = ({category}) => {
     <div className='food-display' id="food-display">
    <h2>Top Dish Near you.</h2>    
    <div className="food-display-list">
-    {food_list.map((item,index)=>{
-        return(
-        <FoodItem key={index} id={item.id}  name={item.name} description={item.description} price={item.price}  image={item.image}   />
-        )
-   })}
+   {food_list.filter(item => category==='All' || category===item.category).map((item) => (
+     <FoodItem key={item._id}  id={item._id} name={item.name} description={item.description}  price={item.price} image={item.image}
+     />
+))}
+
     </div>    
 </div>
   )
