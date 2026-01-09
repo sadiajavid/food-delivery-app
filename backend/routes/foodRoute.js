6c32281cdd145127e10ca38aee94430d28594b80
saida javid <sadiajavid31@gmail.com>
2026-01-09 12:02:31 +0500

    chnges added

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -18,7 +18,7 @@ const upload=multer({storage:storage})
 
 foodRouter.post('/add',upload.single("image"),addFood)
 foodRouter.get('/list',listFood)
-foodRouter.get('/remove',removeFood)
+foodRouter.post('/remove',removeFood)
 
 
 
