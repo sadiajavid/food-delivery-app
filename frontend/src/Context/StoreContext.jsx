153da0093ac19d301e1ac03d63926cae3f843066
saida javid <sadiajavid31@gmail.com>
2026-01-09 17:11:24 +0500

    login and register api integration on frontend

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -4,6 +4,8 @@ import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
+    const url="http://localhost:4000";
+    const [token,setToken]=useState("");
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
@@ -33,7 +35,10 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token ,
+        setToken
     }
     return (
         <StoreContext.Provider value={contextValue}>
